a441f2f63d73d9efc96bcdfa86b361e0795ce977
brunacarolinac <brucarolinac@gmail.com>
2023-07-28 17:42:49 -0300

    sintaxe-basica

---
+++ sintaxe-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,22 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Ola novo teste");
+		
+		int idade;
+		idade = 28;
+		
+		System.out.println(idade);
+		
+		idade = 20 + 10;
+		
+		System.out.println(idade);
+		
+		idade = (7 * 5) + 2;
+		
+		System.out.println(idade); 
+		
+		System.out.println("a idade e " + idade + ", parabens!");
+	}
+}
